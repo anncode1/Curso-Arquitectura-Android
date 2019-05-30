e7d26f87287d9b1ec88ca5dbaa08cf2c68f97ae4
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-05-30 11:55:47 -0500

    ViewModel with View connection

--- app/src/main/java/com/anncode/offersandcoupons/view/MainActivity.kt
+++ app/src/main/java/com/anncode/offersandcoupons/view/MainActivity.kt
@@ -2,14 +2,18 @@ package com.anncode.offersandcoupons.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.anncode.offersandcoupons.R
+import com.anncode.offersandcoupons.model.Coupon
+import com.anncode.offersandcoupons.viewmodel.CouponViewModel
 
 class MainActivity : AppCompatActivity(){
 
 
-    private var rvCoupons: androidx.recyclerview.widget.RecyclerView? = null
+    private var couponViewModel: CouponViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +21,30 @@ class MainActivity : AppCompatActivity(){
         supportActionBar?.hide()
 
         //VIEW
-        rvCoupons = findViewById(R.id.rvCoupons) //UI
-        rvCoupons?.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        setupBindings(savedInstanceState)
 
+
+    }
+
+    fun setupBindings(savedInstanceState: Bundle?){
+        var activityMainBinding: com.anncode.offersandcoupons.databinding.ActivityMainBinding
+        = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        couponViewModel = ViewModelProviders.of(this).get(CouponViewModel::class.java)
+
+        activityMainBinding.setModel(couponViewModel)
+        setupListUpdate()
+
+    }
+
+    fun setupListUpdate(){
         //CallCoupons
+        couponViewModel?.callCoupons()
         //getCoupons - Lista de cupones
-
+        couponViewModel?.getCoupons()?.observe(this, Observer {
+                coupon: List<Coupon> ->
+            Log.w("COUPON", coupon.get(0).title)
+        })
     }
 
 }
